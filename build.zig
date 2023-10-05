cdac754712c58fe1aabbce689c10191c8e465e5e
Luke Boswell <lukewilliamboswell@gmail.com>
2023-10-05 14:42:19 +1100

    clean up build.zig

--- build.zig
+++ build.zig
@@ -16,30 +16,20 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
         .deps = &[_]std.build.ModuleDependency{},
-        // .custom_entrypoint = null,
-        // .roc_app_dylib_path = null,
         .custom_entrypoint = "platform/src/host.zig",
         .roc_app_dylib_path = "rocLovesZig.dylib",
     });
     if (b.args) |args| app.run.addArgs(args);
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&app.run.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
